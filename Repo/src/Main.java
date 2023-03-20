dbe2319b76dba04b067c0ed17c1eb6d7ece9dcfd
TheRealRomy <100058219+TheRealRomy@users.noreply.github.com>
2023-03-20 20:21:42 +0800

    Changed the App file to Main along with its contents

---
+++ Repo/src/Main.java
@@ -0,0 +1,34 @@
+import java.awt.Desktop;
+import java.net.URI;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        int[] testArray = new int[args.length];
+        System.out.print(testArray);
+        
+        for(int i = 0; i< args.length; i++) {
+			System.out.print(args[i]);
+		}
+        // Opens the specific URL in the browser 
+
+        // String url = "https://www.youtube.com";
+    
+        // try {
+        //     // specify the URL you want to open
+        //     URI uri = new URI(url);
+
+        //     // get the desktop object and check if it's supported
+        //     Desktop desktop = Desktop.getDesktop();
+        //     if (desktop.isSupported(Desktop.Action.BROWSE)) {
+        //         // open the default web browser and visit the URL
+        //         desktop.browse(uri);
+        //     } else {
+        //         System.out.println("Desktop browsing is not supported.");
+        //     }
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
+
+    }
+}
