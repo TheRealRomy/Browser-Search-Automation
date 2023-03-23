a7d0b4b7c1db553048357a0ee0794a55cdcf6b41
TheRealRomy <100058219+TheRealRomy@users.noreply.github.com>
2023-03-23 10:03:45 +0800

    Finished Project

--- Repo/src/Main.java
+++ Repo/src/Main.java
@@ -1,29 +1,62 @@
 import java.awt.Desktop;
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-
-        // Open default browser with given URL
+    public static void main(String[] args) throws IOException, URISyntaxException {
+    
         String url = "https://www.google.com/search?q=";
-        Desktop desktop = Desktop.getDesktop();
-        desktop.browse(URI.create(url));
+        String validWebsites[] = {"stackoverflow.com", "reddit.com", "geekforgeeks.com"};
 
-        // Creates an array that copies all of the elements of the args
-        String[] finalQuery = new String[args.length];
 
-        finalQuery = args;
+        // Copies the array of strings from the terminal
+        String query[] = new String[args.length];
 
-        String query = String.join(" ", finalQuery);
+        for (int i = 0; i < args.length; i++) {
+            query[i] = args[i];
+        }
 
-        url = url.concat(query);
-        System.out.print(url);
+        if (query.length == 0) {
+            System.out.print("Error: Please enter a proper search, query, Romy.");
+        } 
+        else {
+            StringBuffer sb = new StringBuffer();
 
-        // Valid websites that only the code will filter the search
-        String[] validWebsites = {  "stackoverflow.com", 
-                                    "reddit.com", 
-                                    "stackexchange.com", 
-                                    "geeksforgeeks.com"};
+            for(int i = 0; i < query.length; i++) {
+                sb.append(query[i] + "+");
+            }
 
+            // Converts that array into a single string 
+            String fixedQuery = sb.toString();
+
+            url = url.concat(fixedQuery);
+            url = url.concat(filteredQuery(validWebsites));
+
+            try {
+                Desktop.getDesktop().browse(new URI(url));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    static String filteredQuery(String validWebsites[]) { 
+        String filter = "(";
+        int index = 0;
+        for (String website : validWebsites) {
+            filter = filter + "site:" + website;
+            index++;
+            if (index == validWebsites.length) {
+                filter = filter + ")";
+            }
+            else {
+                filter = filter + "+OR+";
+            }
+            
+        }
+        
+        return filter;
     }
 }
